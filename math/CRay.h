cd07884acab3c0dc28254c85e45c56f4e7849360
Claire Blackshaw <evilkimau@gmail.com>
2019-12-17 13:53:31 +0000

    Added ray

---
+++ math/CRay.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+#include "CVector3.h"
+
+//#########################################################################
+//	3D Yar Class
+//	Float base, note floating point errors
+//#########################################################################
+class Ray
+{
+public:
+	Ray() : a(CVector3(0,0,0)), b(CVector3(0,0,1)) {}
+	Ray(const Ray& r) : a(r.a), b(r.b) {}
+    Ray(const CVector3& origin, const CVector3& dir) : a(origin), b(dir) {}
+
+	inline CVector3 PointAt(float t) const { return a + b*t; }
+
+	inline const CVector3& origin() const { return a; } 
+    inline const CVector3& dir() const { return b; }
+
+	CVector3 a,b;
+};
+
